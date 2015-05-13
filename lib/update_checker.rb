e68c9ab12ce1eb020e33323ae4524ebcbeadbba6
Matúš Tomlein <matus.tomlein@gmail.com>
2015-05-13 10:43:06 +0200

    Fix a bug in update checker

--- lib/update_checker.rb
+++ lib/update_checker.rb
@@ -4,6 +4,7 @@ class UpdateChecker
   def self.check
     begin
       content = JSON.parse(open('https://dweet.io/get/latest/dweet/for/ddist2015-version').read)
+      return if content['with'] == 404
       newest_version = content['with'].first['content']['version'].to_i
       current_version = File.read('version').to_i
       if newest_version > current_version
